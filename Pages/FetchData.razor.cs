acb681f429cb67184bb3ce10213196cc9ca6f86d
Juris Augskalns <juris.augskalns@gmail.com>
2021-12-15 22:45:51 +0200

    Search

--- Pages/FetchData.razor.cs
+++ Pages/FetchData.razor.cs
@@ -8,6 +8,12 @@ namespace BlazorTest.Pages
     public partial class FetchData
     {
         private List<VirtueRecord> virtues = new List<VirtueRecord>();
+
+        private List<VirtueRecord> filteredVirtues = new List<VirtueRecord>();
+
+        protected SearchModel searchModel { get; set; } = new SearchModel();
+
+
         protected override async Task OnInitializedAsync()
         {
 
@@ -22,10 +28,44 @@ namespace BlazorTest.Pages
             }
         }
 
+        protected void OnSearch()
+        {
+            if (!string.IsNullOrWhiteSpace(searchModel?.Query))
+            {
+                var q = searchModel.Query.ToLowerInvariant();
+                filteredVirtues = virtues.Where(x =>
+                    x.EtapValues.Any(v => v.Contains(q)) ||
+                    x.EtapVirtues.Any(v => v.Contains(q)) ||
+                    x.School2030Values.Any(v => v.Contains(q)) ||
+                    x.School2030Virtues.Any(v => v.Contains(q)))
+                .ToList();
+            }
+        }
+
+        protected void ShowAll()
+        {
+            filteredVirtues = virtues;
+            searchModel.Query = "";
+        }
+
+        protected void ClearResults()
+        {
+            filteredVirtues.Clear();
+            searchModel.Query = "";
+        }
+
         private CsvConfiguration CsvConfig = new CsvConfiguration(new CultureInfo("lv-lv"))
         {
             Delimiter = ",", // Enforce ',' as delimiter
             PrepareHeaderForMatch = header => header.Header.ToLower() // Ignore casing
         };
+
+        
+    }
+
+    public class SearchModel
+    {
+
+        public string Query { get; set; } = "";
     }
 }
